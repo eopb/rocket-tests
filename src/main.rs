53b8be3666011b84ee51ae5839e96bd1cc7508a3
Ethan Brierley <ethanboxx@gmail.com>
2019-07-17 21:10:50 +0100

    github requests

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,34 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket_contrib::serve::StaticFiles;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug, Serialize)]
+struct Repo {
+    name: String,
+    stargazers_count: u32,
+    description: Option<String>,
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/<name>/<age>")]
-fn hello(name: String, age: String) -> String {
-    format!("hi {} {}", name, age)
+#[get("/<user>")]
+fn for_github_user(user: String) -> String {
+    let resp: Vec<Repo> = reqwest::get(&format!("https://api.github.com/users/{}/repos", user))
+        .unwrap()
+        .json()
+        .unwrap();
+    format!("{:#?}", resp)
 }
 
 fn main() {
     rocket::ignite()
         .mount("/", routes![index])
-        .mount("/", routes![hello])
+        .mount("/github", routes![for_github_user])
+        .mount("/public/style", StaticFiles::from("style"))
         .launch();
 }
